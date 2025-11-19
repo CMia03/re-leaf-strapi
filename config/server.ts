5be543f26b3ea9b3e8d3cf343ae3efe68286b23b
CMia03 <38261906+CMia03@users.noreply.github.com>
2025-11-19 16:24:34 +0300

    edit host

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,30 @@
+// export default ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+// });
+
+
+import os from 'os';
+
+// Patch pour éviter l'erreur hostname sur Vercel
+if (!os.hostname) {
+  os.hostname = () => 'localhost';
+} else {
+  const originalHostname = os.hostname;
+  os.hostname = () => {
+    try {
+      return originalHostname();
+    } catch {
+      return 'localhost';
+    }
+  };
+}
+
 export default ({ env }) => ({
-  // host: env('HOST', '0.0.0.0'),
-  host: env('HOST', 'localhost'),
+  host: env('HOST', 'localhost'), // <-- utiliser localhost au lieu de 0.0.0.0
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
